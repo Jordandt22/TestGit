7989715d3b43f1574bc0b0480bfaf5e9db70f5fd
Jordandt22 <jordandt123411@gmail.com>
2025-09-14 17:52:59 -0700

    Updated Delivery Function

--- delivery.js
+++ delivery.js
@@ -3,6 +3,7 @@ import { user } from "./user.js";
 
 function delivery(store, user) {
   console.log("New Delivery for " + user.name + " from " + store.name);
+  console.log("Address: " + store.address);
 }
 
 delivery(stores[0], user);
