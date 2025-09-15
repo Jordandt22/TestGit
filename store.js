835bbe491b3fc3a1c0f460c4fcbdb2652c93a1ac
Jordandt22 <jordandt123411@gmail.com>
2025-09-14 17:23:17 -0700

    New Store File

---
+++ store.js
@@ -0,0 +1,11 @@
+const store = {
+  name: "Papa John's",
+  address: "123 Main St, Anytown, USA",
+  phone: "123-456-7890",
+  email: "info@papajohns.com",
+  website: "www.papajohns.com",
+  hours: "10am-10pm",
+  menu: ["pizza", "pasta", "salad", "dessert"],
+};
+
+console.log(store);
